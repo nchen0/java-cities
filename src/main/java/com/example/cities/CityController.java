3e4294a0b6415d9d9d193de61874a29f3523ed7f
nchen0 <nicky.chen0@gmail.com>
2019-02-27 20:12:20 -0500

    mvp finished

--- src/main/java/com/example/cities/CityController.java
+++ src/main/java/com/example/cities/CityController.java
@@ -3,6 +3,7 @@ package com.example.cities;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,6 +11,7 @@ import java.util.Random;
 
 @Slf4j
 @RestController
+@RequestMapping("/cities")
 public class CityController {
 
     private final CityRepository cityrepos;
@@ -20,27 +22,63 @@ public class CityController {
         this.rt = rt;
     }
 
-    @GetMapping("/cities")
+    @GetMapping("/afford")
     public void findSome()
     {
         ArrayList<City> cities = new ArrayList<City>();
         cities.addAll(cityrepos.findAll());
 
-        for (City c: cities)
-        {
-            int rand = new Random().nextInt(10);
-            boolean randBool = new Random().nextBoolean();
-            final CityMessage message = new CityMessage(c.toString(), rand, randBool);
+        for (City c: cities) {
+            int priority = c.getAffordabilityIndex();
+            boolean secret = new Random().nextBoolean();
+            final CityMessage message = new CityMessage(c.toString(), priority, secret);
 
             log.info("Sending Message...");
-            if (rand <= 5)
-            {
+            if (secret) {
+                rt.convertAndSend(CitiesApplication.SECRET_QUEUE, message);
+            } else if (priority < 6) {
                 rt.convertAndSend(CitiesApplication.QUEUE_CITIES1, message);
+            } else {
+                rt.convertAndSend(CitiesApplication.QUEUE_CITIES2, message);
             }
-            else
-            {
+        }
+    }
+
+    @GetMapping("/homes")
+    public void findHomes() {
+        ArrayList<City> cities = new ArrayList<City>();
+        cities.addAll(cityrepos.findAll());
+
+        for (City c: cities) {
+            int priority = c.getPrice();
+            boolean secret = new Random().nextBoolean();
+            final CityMessage message = new CityMessage(c.toString(), priority, secret);
+
+            if (secret) {
+                rt.convertAndSend(CitiesApplication.SECRET_QUEUE, message);
+            } else if (priority > 200000) {
+                rt.convertAndSend(CitiesApplication.QUEUE_CITIES1, message);
+            } else {
+                rt.convertAndSend(CitiesApplication.QUEUE_CITIES2, message);
+            }
+        }
+    }
+
+    @GetMapping("/names")
+    public void findNames() {
+        ArrayList<City> cities = new ArrayList<City>();
+        cities.addAll(cityrepos.findAll());
+
+        for (City c : cities) {
+            int priority = c.getPrice();
+            boolean secret = new Random().nextBoolean();
+            final CityMessage message = new CityMessage(c.toString(), priority, secret);
+
+            if (secret) {
+                rt.convertAndSend(CitiesApplication.SECRET_QUEUE, message);
+            } else {
                 rt.convertAndSend(CitiesApplication.QUEUE_CITIES1, message);
             }
         }
     }
-}
+};
--- src/main/java/com/example/cities/CityMessageListener.java
+++ src/main/java/com/example/cities/CityMessageListener.java
@@ -9,6 +9,16 @@ import org.springframework.stereotype.Service;
 public class CityMessageListener {
     @RabbitListener(queues = CitiesApplication.QUEUE_CITIES1)
     public void receiveMessage(CityMessage c) {
-        log.info("Received Message: {}", c.toString());
+        log.info("Received Cities 1 Message: {}", c.toString());
+    }
+
+    @RabbitListener(queues = CitiesApplication.QUEUE_CITIES2)
+    public void receiveMessage2(CityMessage c) {
+        log.info("Received Cities 2 Message: {}", c.toString());
+    }
+
+    @RabbitListener(queues = CitiesApplication.SECRET_QUEUE)
+    public void receiveMessageSecret(CityMessage c) {
+        log.info("Received Secret Message: {}", c.toString());
     }
 }
